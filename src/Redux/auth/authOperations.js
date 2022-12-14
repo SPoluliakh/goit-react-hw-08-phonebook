e62d67c2c3a8925912d57d5411c6fc441e8ac1ef
SPoluliakh <S_Poluliakh@ukr.net>
2022-12-14 19:10:53 +0200

    fixed

--- src/Redux/auth/authOperations.js
+++ src/Redux/auth/authOperations.js
@@ -16,13 +16,11 @@ export const register = createAsyncThunk(
 export const logIn = createAsyncThunk(
   'auth/login',
   async (credentials, thunkAPI) => {
-    console.log(credentials);
     try {
       const user = await fetch.login(credentials);
-      console.log(user);
+
       return user;
     } catch (err) {
-      console.log(err.message);
       thunkAPI.rejectWithValue(err.message);
     }
   }
@@ -41,10 +39,10 @@ export const refreshUser = createAsyncThunk(
   async (_, thunkAPI) => {
     // Reading the token from the state via getState()
     const state = thunkAPI.getState();
-    console.log(state.auth.token);
+
     const persistedToken = state.auth.token;
 
-    if (!persistedToken) {
+    if (persistedToken === null) {
       // If there is no token, exit without performing any request
       return thunkAPI.rejectWithValue('Unable to fetch user');
     }
@@ -53,7 +51,7 @@ export const refreshUser = createAsyncThunk(
       // If there is a token, add it to the HTTP header and perform the request
       fetch.setAuthHeader(persistedToken);
       const res = await fetch.refreshUser();
-      return res.data;
+      return res;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
--- src/Redux/auth/authReducer.js
+++ src/Redux/auth/authReducer.js
@@ -5,8 +5,7 @@ const initialState = {
   user: { name: null, email: null },
   token: null,
   isLoggedIn: false,
-  isRefreshing: true,
-  isLoading: false,
+  isRefreshing: false,
 };
 
 export const authSlice = createSlice({
@@ -15,47 +14,49 @@ export const authSlice = createSlice({
 
   extraReducers: builder =>
     builder
-      .addCase(register.pending, (state, _) => {
-        state.isLoading = true;
-      })
+
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload?.user;
         state.token = action.payload?.token;
         state.isLoggedIn = true;
-        state.isLoading = false;
       })
       .addCase(register.rejected, (state, _) => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
-        state.isLoading = false;
       })
-      .addCase(logIn.pending, (state, _) => (state.isLoading = true))
+      .addCase(logIn.pending, (state, _) => state)
       .addCase(logIn.fulfilled, (state, action) => {
-        console.log(action.payload);
         state.user = action.payload?.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
-        state.isLoading = false;
       })
       .addCase(logIn.rejected, (state, _) => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
-        state.isLoading = false;
       })
-      .addCase(logOut.pending, (state, _) => (state.isLoading = true))
+      .addCase(logOut.pending, (state, _) => {
+        state.isLoading = true;
+      })
       .addCase(logOut.fulfilled, (state, _) => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
-        state.isLoading = false;
+
         state.isRefreshing = false;
       })
-      .addCase(refreshUser.pending, (state, _) => (state.isRefreshing = true))
+      .addCase(refreshUser.pending, (state, _) => {
+        state.isRefreshing = true;
+      })
       .addCase(refreshUser.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, (state, _) => {
+        state.user = { name: null, email: null };
+        state.isLoggedIn = false;
         state.isRefreshing = false;
       }),
 });
